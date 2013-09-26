c9ee3d34a534d84d0bf1d47e6a315628ad644bf8
Sandmann79 <sandmann79@gmx.net>
2013-09-26 15:53:44 +0200

    exfat adjustments

--- fs/exfat/exfat_data.h
+++ fs/exfat/exfat_data.h
@@ -69,11 +69,6 @@ extern "C" {
 #define BUF_CACHE_SIZE          256
 #define BUF_CACHE_HASH_SIZE     64
 
-#ifndef CONFIG_EXFAT_DEFAULT_CODEPAGE
-#define CONFIG_EXFAT_DEFAULT_CODEPAGE	437
-#define CONFIG_EXFAT_DEFAULT_IOCHARSET	"utf8"
-#endif
-
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
